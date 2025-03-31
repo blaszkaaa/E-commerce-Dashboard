d16ce72016710a9596f9815e96fab6a5e8579fcf
Blaszka <mateuszniema1@gmail.com>
2025-03-31 12:41:28 +0200

    update 2.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 
 // Pages
@@ -25,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <AnimatePresence mode="wait">
           <motion.div
             initial={{ opacity: 0 }}
@@ -47,7 +46,7 @@ const App = () => (
             </Routes>
           </motion.div>
         </AnimatePresence>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
